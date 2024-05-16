3fdad1ee3e616b077f49a9df7539164088a2a1e5
JardsonVenancio <126803792+jardson123442@users.noreply.github.com>
2024-05-16 16:16:07 -0300

    JwtUserDetailsService Creation

--- src/main/java/com/pb/demoparkapi/repository/UsuarioRepository.java
+++ src/main/java/com/pb/demoparkapi/repository/UsuarioRepository.java
@@ -2,7 +2,14 @@ package com.pb.demoparkapi.repository;
 
 import com.pb.demoparkapi.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
+    Optional<Usuario> findByUsername(String username);
+
+    @Query("select u.role from Usuario u where u.username like :username")
+    Usuario.Role findRoleByUsername(String username);
 }
